428da7c397ea16846310334b2866c32e6a01e483
username40 <52605355+username40@users.noreply.github.com>
2019-11-06 00:33:36 +0300

    add html5/css3 course files

---
+++ GeekBrains/html-css/js/script.js
@@ -0,0 +1,14 @@
+$("#slider").slider({
+    min: 0,
+    max: 1000,
+    values: [0,1000],
+    range: true,
+    stop: function(event, ui) {
+        jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+        jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+    },
+    slide: function(event, ui){
+        jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+        jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+    }
+});
