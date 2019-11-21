fa7b3c9528b525d7f1b7677f720318421a982b9d
username40 <52605355+username40@users.noreply.github.com>
2019-11-21 10:30:50 +0300

    make little pause now comtinue learn vue.js

---
+++ SelfEducation/VueJS/031-register-components-in-vue/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  <h1>{{ msg }}</h1>
+    <app-car/>
+<!--    after all this things just call it-->
+    <app-car-two/>
+  </div>
+</template>
+
+<script>
+  // You can register you components local and use only there where you register it
+  // at first you need do the same that you done like global - write import
+  import Car2 from './Car2'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+    // then you must use special object 'components' and give name of your component
+    components: {
+      appCarTwo: Car2
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ SelfEducation/VueJS/031-register-components-in-vue/Car.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h3>Name: {{ carName }}</h3>
+      <p>Year: {{ carYear }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Car",
+        data() {
+            return {
+                carName: 'Ford',
+                carYear: 2016
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
