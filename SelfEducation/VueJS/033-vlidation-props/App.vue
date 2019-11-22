34170482d2a66547f1034acc2d92db59e3fd599d
username40 <52605355+username40@users.noreply.github.com>
2019-11-22 08:45:59 +0300

    know how to validate props

---
+++ SelfEducation/VueJS/033-vlidation-props/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  <h1>Parent: {{ carName }}</h1>
+<!--    if you need transfer some data into your components you should use props-->
+<!--    use : and then write name of your props (it is match from name of props inside component)-->
+<!--    better if you check it-->
+    <app-car
+      :carName="carName"
+      :carYear="carYear"/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      carName: 'Ford',
+        carYear: 2099,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+    components: {
+    }
+}
+</script>
+
+<style>
+</style>
