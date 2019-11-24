27dc13ed1f05bb79af26e8357692992268d5ea06
username40 <52605355+username40@users.noreply.github.com>
2019-11-24 17:38:14 +0300

    know how to connect elems between yourself

---
+++ SelfEducation/VueJS/036-connect-child-components-between-yourself/Car.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="car">
+    <h3>Name: {{ carName }} / {{ reverseName }}</h3>
+    <p>Year: {{ carYear }}</p>
+    <button @click="changeName">Change name</button>
+    <button @click="changeFunc()">Change from parent</button>
+    <button @click="updateCounter">Update Counter</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Car",
+        // without validate
+        // props: ['carName', 'carYear'],
+        // with validate - it is object, where you can call all parametres that you need
+        props: {
+            carName: {
+                type: String,
+                required: true,
+                default: 'Default Name'
+            },
+            carYear: Number,
+            changeFunc: Function,
+            counter: Number
+        },
+        computed: {
+            reverseName() {
+                return this.carName.split('').reverse().join('')
+            }
+        },
+        data() {
+            return {}
+        },
+        methods: {
+            changeName() {
+                this.carName = 'Mazda'
+                // if you want to transfer something into parent component from child you should use
+                // $emit - first - name of event, second - value of data
+                this.$emit('nameChanged', this.carName)
+                // after you must write about this emit into parent component
+            },
+            updateCounter() {
+                this.$emit('counterUpdated', this.counter + 1)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  .car {
+    border: 1px solid black
+  }
+
+  .car h3 {
+    margin-bottom: 5px;
+  }
+</style>
