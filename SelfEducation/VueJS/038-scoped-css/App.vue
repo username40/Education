ebc118cb7359a8b76fe6763447a54f39a2a1968f
username40 <52605355+username40@users.noreply.github.com>
2019-11-25 19:36:17 +0300

    now i know how to isolate css inside component

---
+++ SelfEducation/VueJS/038-scoped-css/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <app-counter/>
+    <app-car/>
+  </div>
+</template>
+
+<script>
+    import Counter from './Counter'
+    import Car from './Car'
+
+    export default {
+        name: 'app',
+        components: {
+            appCounter: Counter,
+            appCar: Car
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ SelfEducation/VueJS/038-scoped-css/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Counter</h2>
+    <p>Lorem ipsum dolor sit amet.</p>
+  </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Counter"
+    }
+</script>
+
+<style>
+  /*Now all h2 elements will be red color*/
+h2{
+  color: red;
+}
+</style>
