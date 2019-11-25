68c39f57c2e8fd1564ec3023d645da6031d55d03
username40 <52605355+username40@users.noreply.github.com>
2019-11-25 16:26:56 +0300

    know what is and how to use event bus

---
+++ SelfEducation/VueJS/037-eventemitter/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>Parent: {{ carName }}</h1>
+
+    <!--    this is a way to transfer some function iside component-->
+    <!--    bind it-->
+    <!--    write your functions inside methods in this component-->
+    <!--    then write in props inside child component-->
+    <!--    it's a simple-->
+    <app-car
+      :carName="carName"
+      :carYear="carYear"
+      :changeFunc="changeNameToAudi"
+      @nameChanged="carName = $event"
+    @counterUpdated="counter = $event"/>
+    <app-counter/>
+  </div>
+</template>
+
+<script>
+  // This way very difficult - you must generate many actions? create data elem here
+  // in other component create props
+  // in third component - create $emit
+  // Ok now it looks simple, but what you gonna do when you will have big application?
+  // better if you will have event bus...
+    import Counter from './Counter'
+    import Car from './Car'
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                carName: 'Ford',
+                carYear: 2099,
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+        methods: {
+            changeNameToAudi() {
+                this.carName = 'Audi'
+            }
+        },
+        components: {
+            appCounter: Counter,
+            appCar: Car
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ SelfEducation/VueJS/037-eventemitter/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ counter }}</h1>
+</template>
+
+<script>
+    import {EventEmitter} from "./main";
+
+    export default {
+        name: "Counter",
+        data() {
+            return {
+                counter: 0
+            }
+        },
+        created() {
+            // then go to component and write what you want
+            // first write name of emit then payload
+            EventEmitter.$on('counterUpdated', (num) => {
+                this.counter += num
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
