ff42e7366bb84b6fc982b2e18b1d22908789487b
username40 <52605355+username40@users.noreply.github.com>
2019-11-26 16:41:50 +0300

    now i know what is mixins, filters and how to use it and complete this theme

---
+++ SelfEducation/VueJS/049-using-mixins/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="searchName">
+    <ul>
+      <li v-for="item in filteredNames">{{ item }}</li>
+    </ul>
+    <hr>
+    <app-list></app-list>
+  </div>
+</template>
+
+<script>
+    import ListMixin from './listMixin'
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+        mixins: [ListMixin],
+        // for creating filters use a special field
+        // this looks like a creating local directives
+        filters: {
+            lowerCase(value) {
+                return value.toLowerCase()
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
