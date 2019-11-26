3d7af92899338afd2d9796bbc063837b80243194
username40 <52605355+username40@users.noreply.github.com>
2019-11-26 11:23:31 +0300

    know how to use modifiers, but i don't think that is a good idea, may be change my mind later

---
+++ SelfEducation/VueJS/045-modificators/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- after you must use :argName.modifier -->
+    <h2 v-colored:background.font="'red'">{{ title }}</h2>
+    <h2 v-colored:color.delay="'blue'">{{ title }}</h2>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      title: 'Hello! I\'m Vue!',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
