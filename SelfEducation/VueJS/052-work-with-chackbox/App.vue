34ed707f4c4e339dbd829c618b350dcc140174ba
username40 <52605355+username40@users.noreply.github.com>
2019-11-27 09:03:20 +0300

    know what is features of using v-model with checkbox in vue

---
+++ SelfEducation/VueJS/052-work-with-chackbox/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>social network</h2>
+<!--    in work with checkbox v-model has some interesting moments-->
+<!--    you must transfer value of your input inside array in data-->
+    <label><input type="checkbox" value="vk" v-model="social">vkontakte</label>
+    <label><input type="checkbox" value="facebook" v-model="social">facebook</label>
+    <label><input type="checkbox" value="instagram" v-model="social">instagram</label>
+
+    <hr>
+
+    <ul>
+      <li v-for="item in social">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                // you must create array in data
+                // it filled when you choose some checkbox))
+                social: []
+            }
+        },
+    }
+</script>
+
+<style scoped>
+  textarea {
+    height: 100px;
+    width: 400px;
+  }
+/*all you need is write white-space: pre on the your updating element*/
+  p {
+    white-space: pre;
+  }
+</style>
