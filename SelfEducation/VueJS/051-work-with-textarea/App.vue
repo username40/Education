ad30e15f1b74a628c2ff6a31f8daeb2ffb2b26f6
username40 <52605355+username40@users.noreply.github.com>
2019-11-27 08:50:05 +0300

    learned how to work v-model with textarea

---
+++ SelfEducation/VueJS/051-work-with-textarea/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Hello vue!</h1>
+<!--    textarea work with v-model same as input-->
+<!--    but if you push enter and then continue writing your formatting is not showing-->
+<!--    this resolving with css-->
+    <textarea type="text" v-model="msg"></textarea>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: ''
+            }
+        },
+    }
+</script>
+
+<style scoped>
+  textarea {
+    height: 100px;
+    width: 400px;
+  }
+/*all you need is write white-space: pre on the your updating element*/
+  p {
+    white-space: pre;
+  }
+</style>
