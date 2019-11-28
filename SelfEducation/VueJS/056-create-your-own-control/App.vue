456e57c2bba57b124884c4fa4ddb53592aeda43e
username40 <52605355+username40@users.noreply.github.com>
2019-11-28 18:50:19 +0300

    learned how tocreate my own control, also complete theme Forms in vue.js

---
+++ SelfEducation/VueJS/056-create-your-own-control/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+  <h2>Form Inputs</h2>
+    <app-on-off
+      v-model="switched"/>
+    <div>
+      <h3 v-if="switched">Component is enabled</h3>
+      <h3 v-else>Component is disabled</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+  import OnOff from "./OnOff"
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                age: 22,
+                switched: false
+            }
+        },
+        watch: {
+            // if you want watch for something you must name of your function same as data field
+            age(value) {
+                console.log(value)
+                console.log(typeof value)
+            }
+        },
+        components: {
+            appOnOff: OnOff
+        }
+    }
+</script>
+
+<style scoped>
+  textarea {
+    height: 100px;
+    width: 400px;
+  }
+</style>
