f9be1e2a94a1b2d2a952932ec92c6fc63f137b64
username40 <52605355+username40@users.noreply.github.com>
2019-11-28 08:45:18 +0300

    how work with select in vue

---
+++ SelfEducation/VueJS/054-work-with-select/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>social network</h2>
+    <select v-model="social">
+      <option
+        v-for="item in socialNetworks">{{ item }}</option>
+    </select>
+    <hr>
+    <p>{{ social }}</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                defaultOption: 'facebook',
+                social: 'facebook',
+                socialNetworks: ['facebook', 'instagram', 'vk']
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  textarea {
+    height: 100px;
+    width: 400px;
+  }
+</style>
