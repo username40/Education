a5a535a3a2cc468e0d2221df8bb8b5103cef9c55
username40 <52605355+username40@users.noreply.github.com>
2019-12-06 08:54:32 -0300

    after small vacation i'm back and start learn vuelidate for validation forms

---
+++ SelfEducation/VueJS/057-validation-forms-with-vuelidate/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+<!--    At first we need to install vuelidate-->
+<!--    go to: https://vuelidate.js.org/#sub-package-content and install it-->
+    <form class="pt-3">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          v-model="email">
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        email: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
