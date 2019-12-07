3d2932661671e698bd067978eaefaf0c7b957e9d
username40 <52605355+username40@users.noreply.github.com>
2019-12-07 07:50:35 -0300

    learned how to display errors with vuelidate

---
+++ SelfEducation/VueJS/059-visual-display-errors/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <form class="pt-3">
+      <div class="form-group">
+        <label for="email">Email</label>
+<!--bind :class for dynamic display errors-->
+        <input
+          type="email"
+          class="form-control"
+          :class="{'is-invalid': $v.email.$error}"
+          id="email"
+          v-model="email"
+          @blur="$v.email.$touch()"
+        >
+<!--        here you check special fields in vuelidate-->
+        <div class="invalid-feedback" v-if="!$v.email.required">Email field is required</div>
+        <div class="invalid-feedback" v-if="!$v.email.email">This field should be an email</div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  // for validate emails you must import it
+  import { required, email } from 'vuelidate/lib/validators'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        email: ''
+      }
+    },
+    // for using vuelidate you must create a field validations
+    validations: {
+      email: {
+        required,
+        email
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
