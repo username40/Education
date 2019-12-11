fa8ca9483ec019198f79e383d13857c83554989c
username40 <52605355+username40@users.noreply.github.com>
2019-12-11 07:32:37 -0300

    know how to make dynamic routes and delete unused files

---
+++ SelfEducation/VueJS/067-dynamic-routes/pages/Car.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Car id = {{ id }}</h1>
+</template>
+
+<script>
+  export default {
+    name: "Car",
+    data() {
+      return {
+        // id: this.$router.currentRoute.params['id'] this is the same strings...
+        // it takes id from router and put it in the id field inside data
+        id: this.$route.params[id]
+      }
+    },
+    watch: {
+      // but if you need to watch for it you must use $route
+      $route(toR, fromR) {
+        this.id = toR.params['id']
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
