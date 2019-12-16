e4e6715eb701410c1fe5dc36b56f3ff55385b3f3
username40 <52605355+username40@users.noreply.github.com>
2019-12-16 08:53:02 -0300

    learned what is sub-routes

---
+++ SelfEducation/VueJS/069-sub-routes/pages/Car.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Car id = {{ id }}</h1>
+    <button class="btn btn-sm btn-info" @click="goBackToCars">Back</button>
+    <br>
+<!--    for creating sub routes (to get information from the another component) you need use-->
+<!--    router-link and router view to show it-->
+    <router-link
+      class="btn btn-info"
+      tag="button"
+      :to="{name: 'carFull', params: {id: id}}"
+    >Full info</router-link>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Car",
+    data() {
+      return {
+        // id: this.$router.currentRoute.params['id'] this is the same strings...
+        // it takes id from router and put it in the id field inside data
+        id: this.$route.params['id']
+      }
+    },
+    methods: {
+      // write method
+      // and write there special field $router.push
+      // it's looks like a push in the array of browser history new page
+      // and then you will be return on a new page)
+      goBackToCars() {
+        this.$router.push('/cars')
+      }
+    },
+    watch: {
+      // but if you need to watch for it you must use $route
+      $route(toR, fromR) {
+        this.id = toR.params['id']
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ SelfEducation/VueJS/069-sub-routes/pages/CarFull.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h4>Car Name: Ford</h4>
+      <h5>Car Year: 2016</h5>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CarFull"
+    }
+</script>
+
+<style scoped>
+
+</style>
