d32ac6fb129e81b29205ddbace0054028941c921
username40 <52605355+username40@users.noreply.github.com>
2019-12-17 11:29:57 -0300

    learned how to protect routing

---
+++ SelfEducation/VueJS/073-protect-routing/pages/Error.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<!--  this page is our 404 page-->
+    <h1 style="color: red">404 Error</h1>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "Error"
+    }
+</script>
+
+<style scoped>
+
+</style>
