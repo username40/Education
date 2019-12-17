94ce365a5efcd9a44622842d4e74be3a64e75eb9
username40 <52605355+username40@users.noreply.github.com>
2019-12-17 09:59:24 -0300

    now i know how to create redirect

---
+++ SelfEducation/VueJS/072-redirect/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+<!--          so, if you want to styling your links you can use router link-->
+<!--          you can use attr tag for creating valid html-->
+<!--          write active class - he will be use only when you stay on the page which you click-->
+<!--          write exact for full matching routes and stay only one link active-->
+          <router-link tag="li" class="nav-link" exact to="/" active-class="active">
+<!--            also inside you can write any html because inside router-link use slot-->
+            <a class="nav-link">Home</a>
+          </router-link>
+          <router-link tag="li" class="nav-link" to="/cars" active-class="active">
+            <a class="nav-link">Cars</a>
+          </router-link>
+          <router-link tag="li" class="nav-link" to="/car/3" active-class="active">
+            <a class="nav-link">Car 3</a>
+          </router-link>
+          <router-link tag="li" class="nav-link" to="/car/4" active-class="active">
+            <a class="nav-link">Car 4</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+  }
+</script>
+
+<style scoped>
+
+</style>
