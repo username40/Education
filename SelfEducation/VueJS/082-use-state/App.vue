29ef427877c9550c9e24ebe0254d0b8fa5571b32
username40 <52605355+username40@users.noreply.github.com>
2019-12-24 06:51:45 -0300

    learned how to use state

---
+++ SelfEducation/VueJS/082-use-state/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container pt-2">
+    <app-counter/>
+    <app-actions/>
+  </div>
+</template>
+
+<script>
+  import Counter from "./Counter";
+  import Actions from "./Actions";
+
+  export default {
+    name: 'app',
+    components: {
+      appCounter: Counter,
+      appActions: Actions
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
