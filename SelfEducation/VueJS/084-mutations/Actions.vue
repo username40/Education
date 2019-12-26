f56299bde35059ddcd9642a1862e6b99581801d5
username40 <52605355+username40@users.noreply.github.com>
2019-12-26 09:35:24 -0300

    learned how to use mutations

---
+++ SelfEducation/VueJS/084-mutations/Actions.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <button class="btn btn-success" @click="updateCounter(1)">Add</button>
+      <button class="btn btn-danger" @click="updateCounter(-1)">Subtract</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Actions",
+      methods: {
+          updateCounter(val){
+            // this.$store.state.counter += val
+            this.$store.commit('changeCounter', val)
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
