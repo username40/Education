ac90bba9b3c7c2221c54aeb2bee7fd7d86b9a578
username40 <52605355+username40@users.noreply.github.com>
2020-02-11 21:37:41 -0300

    create app - let's get it started

---
+++ SelfEducation/nuxtJS/01-create-app-and-install-bootstrap/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">NuxtJS</a>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Users</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  </header>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
