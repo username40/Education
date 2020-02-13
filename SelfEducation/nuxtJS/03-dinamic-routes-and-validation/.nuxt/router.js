2b3e1ae0bd45e63535acfd76dcca6772289a2a28
username40 <52605355+username40@users.noreply.github.com>
2020-02-13 10:53:57 -0300

    write code about dinamyc routes and how we cn easy way validate it

---
+++ SelfEducation/nuxtJS/03-dinamic-routes-and-validation/.nuxt/router.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _56e0122e = () => interopDefault(import('..\\pages\\about\\index.vue' /* webpackChunkName: "pages_about_index" */))
+const _47f2dcd3 = () => interopDefault(import('..\\pages\\users\\index.vue' /* webpackChunkName: "pages_users_index" */))
+const _0227f7fb = () => interopDefault(import('..\\pages\\users\\_id.vue' /* webpackChunkName: "pages_users__id" */))
+const _6c70e699 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
+
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: decodeURI('/'),
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/about",
+    component: _56e0122e,
+    name: "about"
+  }, {
+    path: "/users",
+    component: _47f2dcd3,
+    name: "users"
+  }, {
+    path: "/users/:id",
+    component: _0227f7fb,
+    name: "users-id"
+  }, {
+    path: "/",
+    component: _6c70e699,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter () {
+  return new Router(routerOptions)
+}
---
+++ SelfEducation/nuxtJS/03-dinamic-routes-and-validation/pages/users/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <h1>Users page</h1>
+    <ul>
+      <li v-for="user in users" :key="user">
+        <a href="#" @click.prevent="goTo(user)">User {{ user }}</a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "users",
+      data: () => ({
+        users: [1,2,3,4,5]
+      }),
+      methods: {
+          goTo(user) {
+            this.$router.push('/users/' + user)
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
