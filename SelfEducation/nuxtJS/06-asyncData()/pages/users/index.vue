503b30f4e7af503f4eb8311b26ad182f697d372a
username40 <52605355+username40@users.noreply.github.com>
2020-02-14 19:29:39 -0300

    install axios and try to use async Data

---
+++ SelfEducation/nuxtJS/06-asyncData()/pages/users/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <h1>{{ pageTitle }}</h1>
+    <ul>
+      <li v-for="user in users" :key="user">
+        <a href="#" @click.prevent="goTo(user)">User {{ user }}</a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "users",
+      asyncData() {
+          return new Promise(resolve => {
+            setTimeout(() => {
+              resolve({
+                users: [1,2,3,4,5]
+              })
+            }, 3000)
+          })
+      },
+      data: () => ({
+        pageTitle: 'Users page'
+      }),
+      methods: {
+          goTo(user) {
+            this.$router.push('/users/' + user)
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
