3c723bc3091fa8553fe82c79fe0d1faecd825d13
viacheslav <mr.v.e.romantsov@gmail.com>
2020-06-15 23:15:49 +0500

    html plugin

--- SelfEducation/webpack/webpack.config.js
+++ SelfEducation/webpack/webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path')
 
 module.exports = {
@@ -5,5 +6,11 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ]
 }
