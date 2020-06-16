c589cdd04216a2e802ed3e8d459774358c5ae0a8
viacheslav <mr.v.e.romantsov@gmail.com>
2020-06-16 10:36:55 +0500

    styles in other file

--- SelfEducation/webpack/webpack.config.js
+++ SelfEducation/webpack/webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path')
 
 module.exports = {
@@ -7,17 +10,25 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin({}),
+      //new UglifyJsPlugin({})
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
     })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   }
