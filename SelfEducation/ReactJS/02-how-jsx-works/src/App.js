337101300689a0623285791d8f657198b4ce20c1
viacheslav <mr.v.e.romantsov@gmail.com>
2020-09-09 23:26:06 +0300

    know how jsx works

---
+++ SelfEducation/ReactJS/02-how-jsx-works/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return React.createElement(
+      'div',
+      {
+        className: 'App'
+      },
+      React.createElement(
+        'h1',
+        null,
+        'Hello React!!!'
+      ),
+
+    )
+  }
+}
+
+export default App;
