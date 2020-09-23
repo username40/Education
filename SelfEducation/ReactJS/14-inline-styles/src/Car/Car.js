80769a0d3c820a8abf5eb6cb77cde03c9722c511
viacheslav <mr.v.e.romantsov@gmail.com>
2020-09-23 19:45:29 +0300

    how i can set inline styles

---
+++ SelfEducation/ReactJS/14-inline-styles/src/Car/Car.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default props => (
+  <div style={{
+    border: '1px solid #ccc',
+    marginBottom: '10px',
+    display: 'block',
+    padding: '10px',
+    boxShadow: '0 4px 5px 0 rgba(0,0,0, .14)',
+    borderRadius: 5
+  }}>
+    <h2>Car name: {props.name}</h2>
+    <strong>car year: {props.year}</strong>
+    <input onChange={props.onChangeName} value={props.name} />
+    <button onClick={props.onDelete}>delete</button>
+  </div>
+)
