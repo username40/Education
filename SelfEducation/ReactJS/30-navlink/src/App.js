4b7b598b2505077de16a9c58d202362a46575221
viacheslav <mr.v.e.romantsov@gmail.com>
2020-11-10 19:55:11 +0300

    know what is navlink

---
+++ SelfEducation/ReactJS/30-navlink/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import './App.scss'
+import About from './About/About'
+import { Route, NavLink } from 'react-router-dom'
+import Cars from './Cars/Cars'
+
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/cars">Cars</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        <Route
+          path="/"
+          exact
+          render={() => <h1>Home page</h1>}
+        />
+        <Route
+          path="/about"
+          component={About}
+        />
+        <Route
+          path="/cars"
+          component={Cars}
+        />
+      </div>
+    );
+  }
+}
+
+export default App
