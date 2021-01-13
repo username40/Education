25ce01c77e3093e3cd90c08da33bbe30eb36f0af
viacheslav <mr.v.e.romantsov@gmail.com>
2021-01-13 23:52:20 +0300

    add methods

---
+++ SelfEducation/ReactJS/37-add-methods/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import './App.scss'
+
+class App extends Component {
+  updateCounter(value) {
+    //this.setState({
+    //  counter: this.state.counter + value
+    //})
+  }
+
+  render() {
+    return (
+      <div className={'App'}>
+        <h1>Счетчик <strong>{this.props.counter}</strong></h1>
+
+        <hr />
+
+        <div className="Actions">
+          <button onClick={this.props.onAdd}>Добавить 1</button>
+          <button onClick={this.props.onSub}>Вычесть 1</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onAdd: () => dispatch({ type: 'ADD' }),
+    onSub: () => dispatch({ type: 'SUB' })
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
---
+++ SelfEducation/ReactJS/37-add-methods/src/redux/rootReducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+  counter: 0
+}
+
+export default function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD':
+      return {
+        counter: state.counter + 1
+      }
+    case 'SUB':
+      return {
+        counter: state.counter - 1
+      }
+  }
+
+  return state
+}
