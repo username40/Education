749511d13facaab1e92ed2481c96e47927154595
viacheslav <mr.v.e.romantsov@gmail.com>
2021-01-15 21:28:40 +0300

    learn redux thunk

---
+++ SelfEducation/ReactJS/42-redux-thunk/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/rootReducer'
+import { Provider } from 'react-redux'
+import { func } from 'prop-types';
+import reduxThunk from 'redux-thunk'
+
+//function loggerMiddleware(store) {
+//  return function (next) {
+//    return function (action) {
+//      const result = next(action)
+//      console.log('middleware', store.getState());
+//      return result
+//    }
+//  }
+//}
+
+const loggerMiddleware = store => next => action => {
+  const result = next(action)
+  console.log('middleware', store.getState());
+  return result
+}
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware, reduxThunk))
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+
+ReactDOM.render(app, document.getElementById('root'));
+registerServiceWorker();
