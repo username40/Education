982c622b196d39d6531efedb737d490cb0d2455b
username40 <mr.v.e.romantsov@gmail.com>
2021-07-22 13:06:54 +0300

    learn functions in ts

--- SelfEducation/typeScript/02-arrays.js
+++
@@ -1,7 +0,0 @@
-var numArray = [1, 2, 3]; // you can tell array what contains inside
-var numStrArr = [1, 2, 3, '123']; // or you can not do this, but in your array can take any data
-var typicalStrNumArr = [1, 2, 3, '123']; // also you can tell what types of data must contains in your array
-var genericNumStrArr = [1, 2, 3, '123']; // also you can use generic 
-var tupleNumStrArr = [1, 2, 3, '123']; // also ts have a tuple but i"m not sure that is a good idea
-// because you can have a situation that you have array with a lot of elements (for ex. 700+) and so what? you will use tuple
-// and write types for all elements?
